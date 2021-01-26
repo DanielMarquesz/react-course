d9395dfe4321c01377bb93001083d5dd3d87e8be
Daniel Marques <daniel.marques142@gmail.com>
2021-01-25 21:05:54 -0300

    feat: learning states

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,14 +1,25 @@
 import CreateTweet from "./components/CreateTweet";
 import TweetList from "./components/TweetList";
+import { useState } from "react";
 
 function App() {
   const name = "Daniel da Conceição Marques";
   const message = "Hello Brother";
+
+  const [namer, setNamer] = useState("Daniel Marques");
+
+  const stayCool = (e) => {
+    setNamer("Lais ALves");
+    console.log(namer);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>{namer}</h1>
       <CreateTweet />
-      <TweetList name={name} message={message} />
+      {/* <TweetList name={name} message={message} /> */}
+
+      <button onClick={stayCool}>Change Name</button>
     </div>
   );
 }
