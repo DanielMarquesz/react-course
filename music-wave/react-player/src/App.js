da50ef718c371a26d9493a1aec9e4e7062c491a1
Daniel da Conceição Marques <daniel.marques@firstdecision.com.br>
2021-02-13 02:10:33 -0300

    adding components

--- music-wave/react-player/src/App.js
+++ music-wave/react-player/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Player from './components/Player'
+import Song from './components/Song'
 
 function App() {
   return (
     <div className="App">
+      <Song/>
+      <Player/>
       <h1>Player Music</h1>
     </div>
   );
