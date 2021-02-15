7e2943e536d9e23d03440dbb93d37c50e3e55550
Daniel Marques <daniel.marques142@gmail.com>
2021-02-14 23:49:27 -0300

    adding data music and more style

--- music-wave/react-player/src/App.js
+++ music-wave/react-player/src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./styles/app.scss"
-import Player from './components/Player'
-import Song from './components/Song'
+import React, { useState } from "react";
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./utils";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong={currentSong} />
+      <Player />
       <h1>Player Music</h1>
     </div>
   );
